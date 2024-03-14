3c7a404273bbc588bcafc2d6d073cb75f16e9166
NreddyKM <86826965+nreddykm@users.noreply.github.com>
2024-03-14 15:27:32 +0200

    Create test-lua.lua

---
+++ test-lua.lua
@@ -0,0 +1,25 @@
+-- Conditional statement in Lua
+local num = 10
+if num > 0 then
+    print("Number is positive")
+else
+    print("Number is non-positive")
+end
+
+-- For loop in Lua to print numbers from 1 to 5
+for i = 1, 5 do
+    print("Number:", i)
+end
+-- Define and call functions in Lua
+function myFunction()
+    print("Hello, world!")
+end
+
+myFunction()
+
+-- Looping concept in Lua
+local count = 1
+while count <= 5 do
+    print("Count:", count)
+    count = count + 1
+end
